33bf4e05b44de20425ae9e9044cdbd1d40428afe
Nathan Cregut <mcr6114@live.com>
2017-03-26 19:39:25 -0500

    Added initial steps to open xml file and find children in xml file

--- ShaderSwap/ShaderSwap/Program.cs
+++ ShaderSwap/ShaderSwap/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace ShaderSwap
 {
@@ -10,6 +12,25 @@ namespace ShaderSwap
     {
         static void Main(string[] args)
         {
+            // Loop through all input xml files
+            foreach (string file in args) {
+                // Attempt to open current file
+                XDocument shaderFile = new XDocument();
+                try {
+                    shaderFile = XDocument.Load(file);
+                }
+                catch (XmlException exception) {
+                    Console.WriteLine("Not a valid xml file. ", exception);
+                }
+
+                // Find the first child node of file
+                XNode child = shaderFile.Root.FirstNode;
+                Console.WriteLine("Made it to here!");
+                // Go through each child node
+                //while (child != null) {
+
+                //}
+            }
         }
     }
 }
